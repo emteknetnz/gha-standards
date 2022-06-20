37cee0d4f25c25c7628c368509f2357d54065b5c
Steve Boyd <emteknetnz@gmail.com>
2022-06-20 23:47:25 +1200

    .

--- ci_crons.php
+++ ci_crons.php
@@ -21,6 +21,9 @@ function cron($hourStrNZST, $day) {
     if ($am) {
         // e.g. NZST 11am SAT = UTC hour 23 SAT
         $hour += 12;
+        if ($hour == 24) {
+            $hour = 0;
+        }
     } else {
         // pm
         // e.g. NZST 11pm SAT = UTC hour 11 SUN 
@@ -42,66 +45,165 @@ $fallback = cron('11pm', SAT);
 
 $crons = [
     cron('3am', DAILY) => [
-        'silvertripe/installer',
-    ],
-    cron('4am', DAILY) => [
         'silvertripe/recipe-kitchen-sink',
     ],
-    cron('11pm', MON) => [
-    ],
-    cron('12pm', MON) => [
-    ],
-    cron('1am', MON) => [
-    ],
-    cron('2am', MON) => [
-    ],
-    cron('11pm', TUE) => [
-    ],
-    cron('12pm', TUE) => [
-    ],
-    cron('1am', TUE) => [
-    ],
-    cron('2am', TUE) => [
-    ],
-    cron('11pm', WED) => [
-    ],
-    cron('12pm', WED) => [
-    ],
-    cron('1am', WED) => [
-    ],
-    cron('2am', WED) => [
-    ],
-    cron('11pm', THU) => [
-    ],
-    cron('12pm', THU) => [
-    ],
-    cron('1am', THU) => [
-    ],
-    cron('2am', THU) => [
-    ],
-    cron('11pm', FRI) => [
-    ],
-    cron('12pm', FRI) => [
-    ],
-    cron('1am', FRI) => [
-    ],
-    cron('2am', FRI) => [
-    ],
-    cron('11pm', SAT) => [
-    ],
-    cron('12pm', SAT) => [
-    ],
-    cron('1am', SAT) => [
-    ],
-    cron('2am', SAT) => [
+    cron('4am', DAILY) => [
+        'silvertripe/installer',
     ],
     cron('11pm', SUN) => [
+        'silverstripe/silverstripe-reports',
+        'silverstripe/silverstripe-siteconfig',
+        'silverstripe/silverstripe-versioned',
+        'silverstripe/silverstripe-versioned-admin',
     ],
-    cron('12pm', SUN) => [
+    cron('12am', MON) => [
+        'silverstripe/comment-notifications',
+        'silverstripe/cwp',
+        'silverstripe/cwp-agencyextensions',
+        'silverstripe/cwp-core',
+    ],
+    cron('1am', MON) => [
+        'silverstripe/cwp-pdfexport',
+        'silverstripe/cwp-search',
+        'silverstripe/cwp-starter-theme',
+        'silverstripe/cwp-watea-theme',
+        'silverstripe/silverstripe-simple',
+    ],
+    cron('2am', MON) => [
+        'silverstripe/doorman',
+        'silverstripe/silverstripe-serve',
+        'silverstripe/silverstripe-graphql-devtools',
+        'silverstripe/silverstripe-testsession',
+        'silverstripe/webpack-config',
+    ],
+    cron('11pm', MON) => [
+        'silverstripe/silverstripe-akismet',
+        'silverstripe/silverstripe-auditor',
+        'silverstripe/silverstripe-admin',
+    ],
+    cron('12am', TUE) => [
+        'silverstripe/silverstripe-asset-admin',
+        'silverstripe/silverstripe-assets',
+        'silverstripe/silverstripe-blog',
+    ],
+    cron('1am', TUE) => [
+        'silverstripe/silverstripe-campaign-admin',
+        'silverstripe/silverstripe-ckan-registry',
+        'silverstripe/silverstripe-cms',
+    ],
+    cron('2am', TUE) => [
+        'silverstripe/silverstripe-config',
+        'silverstripe/silverstripe-errorpage',
+        'silverstripe/silverstripe-framework',
+    ],
+    cron('11pm', TUE) => [
+        'silverstripe/silverstripe-graphql',
+        'silverstripe/silverstripe-comments',
+        'silverstripe/silverstripe-content-widget',
+        'silverstripe/silverstripe-contentreview',
+    ],
+    cron('12am', WED) => [
+        'silverstripe/silverstripe-crontask',
+        'silverstripe/silverstripe-documentconverter',
+        'silverstripe/silverstripe-elemental',
+    ],
+    cron('1am', WED) => [
+        'silverstripe/silverstripe-elemental-bannerblock',
+        'silverstripe/silverstripe-elemental-fileblock',
+        'silverstripe/silverstripe-environmentcheck',
+    ],
+    cron('2am', WED) => [
+        'silverstripe/silverstripe-externallinks',
+        'silverstripe/silverstripe-fulltextsearch',
+        'silverstripe/silverstripe-gridfieldqueuedexport',
+    ],
+    cron('11pm', WED) => [
+        'silverstripe/silverstripe-html5',
+        'silverstripe/silverstripe-hybridsessions',
+        'silverstripe/silverstripe-iframe',
+        'silverstripe/silverstripe-ldap',
+
+    ],
+    cron('12am', THU) => [
+        'silverstripe/silverstripe-lumberjack',
+        'silverstripe/silverstripe-mimevalidator',
+        'silverstripe/silverstripe-postgresql',
+        'silverstripe/silverstripe-realme',
+    ],
+    cron('1am', THU) => [
+        'silverstripe/silverstripe-session-manager',
+        'silverstripe/recipe-authoring-tools',
+        'silverstripe/recipe-blog',
+    ],
+    cron('2am', THU) => [
+        'silverstripe/recipe-ccl',
+        'silverstripe/recipe-cms',
+        'silverstripe/recipe-collaboration',
+    ],
+    cron('11pm', THU) => [
+        'silverstripe/recipe-content-blocks',
+        'silverstripe/recipe-core',
+        'silverstripe/recipe-form-building',
+        'silverstripe/recipe-reporting-tools',
+
+    ],
+    cron('12am', FRI) => [
+        'silverstripe/recipe-plugin',
+        'silverstripe/recipe-services',
+        'silverstripe/recipe-solr-search',
+    ],
+    cron('1am', FRI) => [
+        'silverstripe/silverstripe-registry',
+        'silverstripe/silverstripe-restfulserver',
+        'silverstripe/silverstripe-securityreport',
+    ],
+    cron('2am', FRI) => [
+        'silverstripe/silverstripe-segment-field',
+        'silverstripe/silverstripe-selectupload',
+        'silverstripe/silverstripe-sharedraftcontent',
+    ],
+    cron('11pm', FRI) => [
+        'silverstripe/silverstripe-sitewidecontent-report',
+        'silverstripe/silverstripe-spamprotection',
+        'silverstripe/silverstripe-spellcheck',
+        'silverstripe/silverstripe-subsites',
+    ],
+    cron('12am', SAT) => [
+        'silverstripe/silverstripe-tagfield',
+        'silverstripe/silverstripe-taxonomy',
+        'silverstripe/silverstripe-textextraction',
+        'silverstripe/silverstripe-userforms',
+    ],
+    cron('1am', SAT) => [
+        'silverstripe/silverstripe-widgets',
+        'silverstripe/silverstripe-mfa',
+        'silverstripe/silverstripe-totp-authenticator',
+    ],
+    cron('2am', SAT) => [
+        'silverstripe/silverstripe-webauthn-authenticator',
+        'silverstripe/silverstripe-login-forms',
+        'silverstripe/silverstripe-security-extensions',
+    ],
+    cron('11pm', SAT) => [
+        'silverstripe/silverstripe-upgrader',
+        'silverstripe/silverstripe-versionfeed',
+        'silverstripe/sspak',
+        'silverstripe/vendor-plugin',
+    ],
+    cron('12am', SUN) => [
+        'symbiote/silverstripe-advancedworkflow',
+        'symbiote/silverstripe-gridfieldextensions',
+        'symbiote/silverstripe-multivaluefield',
+        'symbiote/silverstripe-queuedjobs',
     ],
     cron('1am', SUN) => [
+        'silverstripe/cow',
+        'silverstripe/eslint-config',
+        'silverstripe/MinkFacebookWebDriver',
     ],
     cron('2am', SUN) => [
+        'silverstripe/recipe-testing',
+        'silverstripe/silverstripe-behat-extension',
     ],
 ];
 
